217f1cc7ebf66d8f5434aeffc28519f9860f78f4
y <1049981896@qq.com>
2019-07-02 10:57:17 +0000

    避免冲突

--- server/index.js
+++ server/index.js
@@ -16,7 +16,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(api)
 // 访问静态资源文件 这里是访问所有dist目录下的静态资源文件
 app.use(express.static(path.resolve(__dirname, '../dist')))
-
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('X-Powered-By', '3.2.1')
+  res.header('Content-Type', 'application/json;charset=utf-8')
+  if (req.method === 'OPTIONS') res.send(200) /* 让options请求快速返回 */
+  else next()
+})
 // 因为是单页应用 所有请求都走/dist/index.html
 app.get('*', (req, res) => {
   const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8')
@@ -25,4 +34,4 @@ app.get('*', (req, res) => {
 // 监听8088端口
 app.listen(8081)
 
-console.log('success listen 95.179.233.238:8088...')
+console.log('success listen 95.179.233.238:8081...')
